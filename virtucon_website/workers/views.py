65641d34d8b0949b1dabacc53be16baffd208f3d
Puyi24 <noamcule@gmail.com>
2022-06-26 01:45:11 +0300

    4th upload - all departments shown in interface

--- virtucon_website/workers/views.py
+++ virtucon_website/workers/views.py
@@ -5,10 +5,10 @@ from .models import Job, Department, Worker
 def workers_list(request):
     ceo = Worker.objects.get(job=1)
     managers = Worker.objects.filter(job=2)
-    board = Worker.objects.filter(department=1)
-    finance = Worker.objects.filter(department=2)
-    attack_defence = Worker.objects.filter(department=3)
-    technology = Worker.objects.filter(department=4)
+    board = Worker.objects.filter(department=1).exclude(id=1)
+    finance = Worker.objects.filter(department=2).exclude(job=2)
+    attack_defence = Worker.objects.filter(department=3).exclude(job=2)
+    technology = Worker.objects.filter(department=4).exclude(job=2)
     return render(request, 'workers_list.html', {'ceo': ceo,
                                                  'managers': managers,
                                                  'board': board,
